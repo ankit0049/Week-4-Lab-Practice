3a72539aada6d1682b5e66e2b772d62852bad5ed
ankit_049 <ankitrajputji00@gmail.com>
2025-02-13 20:32:54 +0530

    [Ankit Rajput] Add. Solution of Remove Duplicates While Preserving Order

--- Day02-Lab-Practice/com/listinterfaceproblems/removeduplicateswhilepreservingorder/RemoveDuplicates.java
+++ Day02-Lab-Practice/com/listinterfaceproblems/removeduplicateswhilepreservingorder/RemoveDuplicates.java
@@ -2,13 +2,10 @@ package com.listinterfaceproblems.removeduplicateswhilepreservingorder;
 
 import java.util.*;
 
-// Class to remove duplicate elements from a list while maintaining the original order.
-
+// Create a Class to remove duplicate elements from a list while maintaining the original order.
 public class RemoveDuplicates {
 
-    // Removes duplicate elements from the list while maintaining the order of the first occurrence.
-
-
+    // Create a method to Removes duplicate elements from the list while maintaining the order of the first occurrence.
     public static List<Integer> removeDuplicates(List<Integer> list) {
 	   // Create a LinkedHashSet to remove duplicates while maintaining insertion order
 	   Set<Integer> set = new LinkedHashSet<>();
