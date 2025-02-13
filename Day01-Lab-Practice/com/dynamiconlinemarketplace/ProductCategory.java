abe8f7846b3ad96a1c9af28b456365caa446489a
ankit_049 <ankitrajputji00@gmail.com>
2025-02-13 20:06:40 +0530

    [Ankit Rajput] Add. Solution of  Dynamic Online Marketplace

--- Day01-Lab-Practice/com/dynamiconlinemarketplace/ProductCategory.java
+++ Day01-Lab-Practice/com/dynamiconlinemarketplace/ProductCategory.java
@@ -1,6 +1,6 @@
 package com.dynamiconlinemarketplace;
 
-// Defining an abstract class to represent a product category
+// Creating abstract class to represent a product category
 abstract class ProductCategory {
     // Declaring a protected variable to store the category name
     public String categoryName;
