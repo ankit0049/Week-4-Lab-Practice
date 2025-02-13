2917e8d512369e1c69911285b6d942954b2af7ca
ankit_049 <ankitrajputji00@gmail.com>
2025-02-13 20:08:30 +0530

    [Ankit Rajput] Add. Solution of Personalized Meal Plan Generator

--- Day01-Lab-Practice/com/personalisedmealgenerator/PersonalizedMealPlanSystem.java
+++ Day01-Lab-Practice/com/personalisedmealgenerator/PersonalizedMealPlanSystem.java
@@ -15,23 +15,23 @@ public class PersonalizedMealPlanSystem {
 	   Meal<KetoMeal> ketoPlan = MealUtils.generateMealPlan("Keto", ketoMeal);
 	   Meal<HighProteinMeal> highProteinPlan = MealUtils.generateMealPlan("High-Protein", proteinMeal);
 
-	   // Displaying meal plan details individually
-//	   System.out.println("Meal Plan Details:");
-//	   System.out.println(vegetarianPlan.getFullMealPlanInfo());
-//	   System.out.println(veganPlan.getFullMealPlanInfo());
-//	   System.out.println(ketoPlan.getFullMealPlanInfo());
-//	   System.out.println(highProteinPlan.getFullMealPlanInfo());
-//
-//	   // Storing all meals in a list using wildcards
-//	   List<MealPlan> allMeals = new ArrayList<>();
-//	   allMeals.add(vegMeal);
-//	   allMeals.add(veganMeal);
-//	   allMeals.add(ketoMeal);
-//	   allMeals.add(proteinMeal);
-//
-//	   // Displaying details of all meal plans using the utility method
-//	   System.out.println("\nDisplaying All Meal Plans:");
-//	   MealUtils.displayAllMeals(allMeals);
+	  // Displaying meal plan details individually
+	   System.out.println("Meal Plan Details:");
+	   System.out.println(vegetarianPlan.getFullMealPlanInfo());
+	   System.out.println(veganPlan.getFullMealPlanInfo());
+	   System.out.println(ketoPlan.getFullMealPlanInfo());
+	   System.out.println(highProteinPlan.getFullMealPlanInfo());
+
+	   // Storing all meals in a list using wildcards
+	   List<MealPlan> allMeals = new ArrayList<>();
+	   allMeals.add(vegMeal);
+	   allMeals.add(veganMeal);
+	   allMeals.add(ketoMeal);
+	   allMeals.add(proteinMeal);
+
+	   // Displaying details of all meal plans using the utility method
+	   System.out.println("\nDisplaying All Meal Plans:");
+	   MealUtils.displayAllMeals(allMeals);
 
 
 	   System.out.println(vegetarianPlan.getMeal());
